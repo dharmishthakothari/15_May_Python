a6a5e949e25a4bd58470c7510cbf399b3e8d88cf
dharmishthakothari <dharmishtha.only@gmail.com>
2025-05-26 11:29:04 +0530

    Files moved to Folder

--- lst_example4.py
+++
@@ -1,7 +0,0 @@
-lst_numbers = [12,34,67,45,22,65]
-lst_even_numbers=[34,56,78,90]
-for i in lst_numbers:
-    if i%2==0:
-        print(i)
-lst_whole = lst_numbers+lst_even_numbers
-print(lst_whole)
