113149612ec6a90fb7d1cbb8cbbcb9813b4ce093
dharmishthakothari <dharmishtha.only@gmail.com>
2025-05-26 11:29:54 +0530

    Files moved to Folder

--- lst_example11.py
+++
@@ -1,9 +0,0 @@
-lst_numbers=[11,22,33,44,55,66,77,88,99]
-print("Original List ",lst_numbers)
-lst_numbers.insert(20,90)
-print("After item added ",lst_numbers)
-lst_numbers.insert(len(lst_numbers),78)
-print("Item added at last ",lst_numbers)
-
-lst_numbers[2]=9999
-print("Item added at 4th position ",lst_numbers)
