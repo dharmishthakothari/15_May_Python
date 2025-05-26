ac37b17b61647d9dc60a32e9794f57a87e39a80a
dharmishthakothari <dharmishtha.only@gmail.com>
2025-05-26 11:37:09 +0530

    Files moved to Folder

--- tpl_example2.py
+++
@@ -1,6 +0,0 @@
-tpl_numbers=(12,45,333,55,67,88)
-print(max(tpl_numbers),min(tpl_numbers),sum(tpl_numbers))
-lst_city=['Ahmedabad','Baroda','Surat','Rajkot','Abcde','Zurich']
-print(max(lst_city,key=len),min(lst_city))
-lst_numbers=[23,34,55,66,20]
-print(sum(lst_numbers))
