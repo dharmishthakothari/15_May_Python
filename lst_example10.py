b6aee58d71f9c2ffb38ecccb131eb0e5ff550b20
dharmishthakothari <dharmishtha.only@gmail.com>
2025-05-26 11:25:40 +0530

    Files moved to Folder

--- lst_example10.py
+++
@@ -1,6 +0,0 @@
-lst_friuts=['Apple','banana','Orange']
-print("Items in List ",lst_friuts)
-lst_friuts.append('Chikoo')
-print("After item addded ",lst_friuts)
-lst_friuts.append("Mango")
-print("After another added ",lst_friuts)
