5b072b014b992049755c24c2c166e360d1f2946c
dharmishthakothari <dharmishtha.only@gmail.com>
2025-05-26 11:27:20 +0530

    Files moved to Folder

--- lst_example18.py
+++
@@ -1,8 +0,0 @@
-lst_numbers = [23,34,12,45,67,88]
-lst_even_numbers=[]
-# for i in lst_numbers:
-#     if i%2==0:
-#         lst_even_numbers.append(i)
-
-lst_even_numbers =[i for i in lst_numbers if i%2==0]
-print(lst_even_numbers)
