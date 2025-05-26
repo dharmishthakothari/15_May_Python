81edcc406d0a9ab4b4740fe5597693997ca38c92
dharmishthakothari <dharmishtha.only@gmail.com>
2025-05-26 11:30:18 +0530

    Files moved to Folder

--- lst_example5.py
+++
@@ -1,9 +0,0 @@
-lst_city=['Ahemdabad','baroda','surat','Rajkot']
-for i in lst_city:
-    i=i.capitalize()
-    print(i)
-
-for i in range(len(lst_city)):
-    lst_city[i]=lst_city[i].capitalize()
-
-print("City List ",lst_city)
