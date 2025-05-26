6db6e2580ae5e5835a63a1be418ef4ec3e9db1d4
dharmishthakothari <dharmishtha.only@gmail.com>
2025-05-26 11:31:07 +0530

    Files moved to Folder

--- lst_example7.py
+++
@@ -1,5 +0,0 @@
-lst_city=['Ahmedabad','Baroda','Surat','Rajkot','Mahesana','Jamnagar']
-lst_sorted_city=sorted(lst_city)
-print(lst_sorted_city)
-lst_desc_city=sorted(lst_city,reverse=True)
-print(lst_desc_city)
