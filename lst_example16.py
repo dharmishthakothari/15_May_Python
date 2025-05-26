181afaf7013d4ebe43708c6dbadca5b62a9022b6
dharmishthakothari <dharmishtha.only@gmail.com>
2025-05-26 11:33:53 +0530

    Files moved to Folder

--- lst_example16.py
+++
@@ -1,8 +0,0 @@
-lst_numbers=[1,2,3,4,5,6]
-lst_new_list=[]
-count=0
-# for i in lst_numbers:
-#     lst_new_list.insert(count,i*i*i)
-#     count+=1
-lst_new_list=[i*i for i in lst_numbers]
-print(lst_new_list)
