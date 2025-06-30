cf0f0a79d7d6002486a998a80f21d1fa743b24e0
dharmishthakothari <dharmishtha.only@gmail.com>
2025-06-30 09:11:00 +0530

    Add files via upload

---
+++ OOPS/db_example.py
@@ -0,0 +1,21 @@
+import sqlite3
+conn=sqlite3.connect("mydb.db")
+cur=conn.cursor()
+query="create table if not exists person1(id integer primary key AUTOINCREMENT,name text,age integer)"
+c=cur.execute(query)
+if c:
+    print("Table created successfully...")
+
+name=input("Please enter username ")
+age=int(input("Please enter age"))
+
+c=cur.execute("insert into person1(name,age) values (?,?)",(name,age))
+if c:
+     print("Data inserted successfully...")
+conn.commit()
+query="select * from person1"
+c=cur.execute(query)
+
+lst_person=c.fetchall()
+for i in lst_person:
+    print(i)
